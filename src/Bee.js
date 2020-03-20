8788244f97b327934d1486effc86879ebdf5baa3
Bouabda <abdaoui.wala@gmail.com>
2020-03-20 18:51:29 +0100

    bees sprint final code

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+
+  Grub.call (this , 5 ,"yellow");
+  this.job = "keep on growing";
+
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee ;
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,19 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this)
+  this.age = 40;
+  this.color = "grey";
+  this.job = "gamble";
+  this.canFly = false;
+  this.treasureChest=[]
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+
+RetiredForagerBee.prototype.forage = function(treasure){
+  return "I am too old, let me play cards instead";
+}
+RetiredForagerBee.prototype.gamble = function(treasure){
+  ForagerBee.prototype.forage.call(this);
+}
